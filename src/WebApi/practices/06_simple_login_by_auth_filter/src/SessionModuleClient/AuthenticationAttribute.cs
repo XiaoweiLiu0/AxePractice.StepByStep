bd8867cee15830e6c42d6b0da10caf62e31b3b0a
Liu Xia <lxconan@gmail.com>
2017-08-25 15:11:00 +0800

    [liuxia] Add webapi practice to use AuthenticationFilter rather than ActionFilter.

--- src/WebApi/practices/06_simple_login_by_auth_filter/src/SessionModuleClient/AuthenticationAttribute.cs
+++ src/WebApi/practices/06_simple_login_by_auth_filter/src/SessionModuleClient/AuthenticationAttribute.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Http.Dependencies;
 using System.Web.Http.Filters;
 
 namespace SessionModuleClient
@@ -17,8 +21,6 @@ namespace SessionModuleClient
             HttpAuthenticationContext context,
             CancellationToken cancellationToken)
         {
-            #region Please implement the following method
-
             /*
              * We need to create IPrincipal from the authentication token. If
              * we can retrive user session, then the structure of the IPrincipal
@@ -32,47 +34,29 @@ namespace SessionModuleClient
              * If user session cannot be retrived, then the context principal
              * should be an empty ClaimsPrincipal (unauthenticated).
              */
-
             if (context == null) { return; }
+            HttpRequestMessage request = context.Request;
+            string token = GetSessionToken(request);
+            if (token == null) { return; }
 
-            var sessionToken = GetSessionToken(context.Request);
-            var session = await GetSessionAsync(context, cancellationToken, sessionToken);
-            if (session == null)
-            {
-                context.Principal = new ClaimsPrincipal();
-                return;
-            }
-
-            SetAuthenticatedPrincipal(context, sessionToken, session);
-
-            #endregion
-        }
-
-        void SetAuthenticatedPrincipal(HttpAuthenticationContext context, string token, UserSessionDto session)
-        {
-            context.Principal = new ClaimsPrincipal(new ClaimsIdentity(new []
-            {
-                new Claim("token", token),
-                new Claim("userFullName", session.UserFullname)
-            }, "custom_authentication_type"));
-        }
-
-        private async UserSessionDto GetSessionAsync(HttpAuthenticationContext context, CancellationToken cancellationToken, string sessionToken)
-        {
-            throw new NotImplementedException();
-        }
-
-        string GetSessionToken(HttpRequestMessage contextRequest)
-        {
-            throw new NotImplementedException();
+            UserSessionDto session = await GetSession(
+                context,
+                cancellationToken,
+                token);
+            if (session == null) { return; }
+            context.Principal = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new []
+                    {
+                        new Claim("token", token),
+                        new Claim("userFullName", session.UserFullname), 
+                    }, "custom_authentication"));
         }
 
         public Task ChallengeAsync(
             HttpAuthenticationChallengeContext context,
             CancellationToken cancellationToken)
         {
-            #region Please implement the following method
-
             /*
              * The challenge method will try checking the configuration of
              * RedirectToLoginOnChallenge property. If the value is true,
@@ -80,14 +64,56 @@ namespace SessionModuleClient
              * And if the value is false, then simply keeps the original
              * response.
              */
-
             if (RedirectToLoginOnChallenge)
             {
-                context.Result = new RedirectToLoginPageIfUnauthorizedResult(context.Request, context.Result);
+                context.Result = new RedirectToLoginPageIfUnauthorizedResult(
+                    context.Request, context.Result);
             }
-            return Task.CompletedTask;
 
-            #endregion
+            return Task.CompletedTask;
+        }
+
+        static async Task<UserSessionDto> GetSession(
+            HttpAuthenticationContext context,
+            CancellationToken cancellationToken,
+            string token)
+        {
+            IDependencyScope scope = context.Request.GetDependencyScope();
+            if (scope == null) { throw new InvalidOperationException("Cannot get dependency scope.");}
+            var client = (HttpClient) scope.GetService(typeof(HttpClient));
+            if (client == null) { throw new InvalidOperationException("Cannot resolve http client.");}
+            Uri requestUri = context.Request.RequestUri;
+            HttpResponseMessage response = await client.GetAsync(
+                $"{requestUri.Scheme}://{requestUri.UserInfo}{requestUri.Authority}/session/{token}",
+                cancellationToken);
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+
+            var session = await response.Content.ReadAsAsync<UserSessionDto>(
+                context.ActionContext.ControllerContext.Configuration.Formatters,
+                cancellationToken);
+            return session;
+        }
+
+        static string GetSessionToken(HttpRequestMessage request)
+        {
+            CookieState sessionCookie = GetSessionCookie(request);
+            string token = sessionCookie?.Value;
+            return string.IsNullOrEmpty(token) ? null : token;
+        }
+
+        static CookieState GetSessionCookie(HttpRequestMessage request)
+        {
+            const string sessionTokenKey = "X-Session-Token";
+            Collection<CookieHeaderValue> cookieHeaderValues =
+                request.Headers.GetCookies(sessionTokenKey);
+            CookieState sessionCookie = cookieHeaderValues
+                .Where(chv => chv.Expires == null || chv.Expires > DateTimeOffset.Now)
+                .SelectMany(chv => chv.Cookies)
+                .FirstOrDefault(c => c.Name == sessionTokenKey);
+            return sessionCookie;
         }
     }
 }
--- src/WebApi/practices/06_simple_login_by_auth_filter/src/SessionModuleClient/AuthorizationRequiredAttribute.cs
+++ src/WebApi/practices/06_simple_login_by_auth_filter/src/SessionModuleClient/AuthorizationRequiredAttribute.cs
@@ -15,23 +15,28 @@ namespace SessionModuleClient
             HttpActionContext actionContext, 
             CancellationToken cancellationToken)
         {
-            #region Please implement the method
-
             /*
              * This authorization attribute will try checking if IPrincipal is valid.
              * If it is not valid, set the response to an unauthorized status. That
              * means, all users that is authenticated is allowd to access resources
              * annotated by this attribute.
              */
-
-            var principal = actionContext.RequestContext.Principal as ClaimsPrincipal;
-            if (principal?.Identity == null || principal.Identity.IsAuthenticated)
+            var principal = actionContext.RequestContext.Principal
+                as ClaimsPrincipal;
+            var identity = principal?.Identity as ClaimsIdentity;
+            if (identity == null || !identity.IsAuthenticated)
             {
-                actionContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                SetAsUnauthorized(actionContext);
+                return Task.CompletedTask;
             }
 
             return Task.CompletedTask;
-            #endregion
+        }
+
+        static void SetAsUnauthorized(HttpActionContext actionContext)
+        {
+            actionContext.Response = new HttpResponseMessage(
+                HttpStatusCode.Unauthorized);
         }
     }
 }
